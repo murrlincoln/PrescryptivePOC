a9a13deccdd2bc76fe60e142b36e6fd301ae00be
murrlincoln <birdbamboo22@gmail.com>
2021-08-05 17:35:16 -0400

    Formatting, bug fixing

--- Truffle/contracts/PrescryptiveSmartContract.sol
+++ Truffle/contracts/PrescryptiveSmartContract.sol
@@ -1,64 +1,67 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.6;
-import '../../node_modules/@openzeppelin/contracts/access/AccessControl.sol'; //allows for different roles to be created
-import '../../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol'; //the ERC20 interface, so that we can transfer tokens to and from smart contract
+import "../../node_modules/@openzeppelin/contracts/access/AccessControl.sol"; //allows for different roles to be created
+import "../../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol"; //the ERC20 interface, so that we can transfer tokens to and from smart contract
 
 contract PrescryptiveSmartContract is AccessControl {
-
     //Role used for adding a withdrawer
     bytes32 public constant WITHDRAW_ROLE = keccak256("WITHDRAW_ROLE");
 
     //Role for the financial team who will confirm or deny tx
-    bytes32 public constant CONFIRM_WITHDRAW_ROLE = keccak256("CONFIRM_WITHDRAW_ROLE");
+    bytes32 public constant CONFIRM_WITHDRAW_ROLE =
+        keccak256("CONFIRM_WITHDRAW_ROLE");
 
     address erc20Contract; //the contract for the token/stablecoin
 
-    ERC20 public stablecoin;
+    ERC20 public stablecoin; //the interface for accessing stablecoin
 
-    bool private withdrawInitated = false; //tells if the withdraw has been initated or not
-    uint public withdrawValue; //the value of the withdrawal
+    bool private withdrawInitated; //tells if the withdraw has been initated or not
+    uint256 public withdrawValue; //the value of the withdrawal
     address public withdrawAddress; //the address of the withdrawal
     address public owner; //DEFAULT_ADMIN_ROLE address
 
     constructor(address _erc20) {
         //make the sender (the payer) both the admin and have withdraw rights
-        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender); 
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _setupRole(WITHDRAW_ROLE, msg.sender);
+        _setupRole(CONFIRM_WITHDRAW_ROLE, msg.sender);
         owner = msg.sender;
 
         //add the interaction interface with the token
         stablecoin = ERC20(_erc20);
         erc20Contract = _erc20;
-
     }
 
-    //depending on setup of the smart contract, the withdraw role may be unnecessary, 
+    //depending on setup of the smart contract, the withdraw role may be unnecessary,
     //if there is only one entity who should have access
 
     /**
      * @dev - Changes the owner to a new address
      */
-    function changeOwner(address _newOwner) public onlyRole(DEFAULT_ADMIN_ROLE) {
+    function changeOwner(address _newOwner)
+        public
+        onlyRole(DEFAULT_ADMIN_ROLE)
+    {
         grantRole(DEFAULT_ADMIN_ROLE, _newOwner);
         revokeRole(DEFAULT_ADMIN_ROLE, owner);
         owner = _newOwner;
     }
 
     /**
-    * @dev Adds another withdrawer role to the smart contract, which can withdraw funds and do nothing else
+     * @dev Adds another withdrawer role to the smart contract, which can withdraw funds and do nothing else
      */
     function addWithdrawer(address _newWithdrawer) public {
         grantRole(WITHDRAW_ROLE, _newWithdrawer);
     }
-
+    
     /**
-     * @dev removes the withdraw role from an address, meaning they can no longer withdraw funds
+     * @dev removes the confirmer role from an address, meaning they can no longer confirm withdraw of funds
      */
-    function removeConfirmer(address _newConfirmer) public {
-        revokeRole(CONFIRM_WITHDRAW_ROLE, _newConfirmer);
+    function removeWithdrawer(address _withdrawer) public {
+        revokeRole(CONFIRM_WITHDRAW_ROLE, _withdrawer);
     }
 
-
+    //Should this be callable by WITHDRAW_ROLE, or require admin?
     /**
      * @dev Adds another confirmer role to the smart contract, which can confirm withdraw of funds and do nothing else
      */
@@ -67,10 +70,10 @@ contract PrescryptiveSmartContract is AccessControl {
     }
 
     /**
-     * @dev removes the confirmer role from an address, meaning they can no longer confirm withdraw of funds
+     * @dev removes the withdraw role from an address, meaning they can no longer withdraw funds
      */
-    function removeWithdrawer(address _withdrawer) public {
-        revokeRole(CONFIRM_WITHDRAW_ROLE, _withdrawer);
+    function removeConfirmer(address _confirmer) public {
+        revokeRole(CONFIRM_WITHDRAW_ROLE, _confirmer);
     }
 
     /**
@@ -85,11 +88,13 @@ contract PrescryptiveSmartContract is AccessControl {
         //TODO - Add Aave integration (not possible until testnet release)
     }
 
-
     /**
      * @dev - The first step in the withdrawal process, sets the address to pay and the value, and also tells the confirmer that everything is ready to check
      */
-    function initiateWithdraw(uint256 _value, address _toPay) public onlyRole(WITHDRAW_ROLE) {
+    function initiateWithdraw(uint256 _value, address _toPay)
+        public
+        onlyRole(WITHDRAW_ROLE)
+    {
         withdrawInitated = true;
 
         withdrawAddress = _toPay;
@@ -98,33 +103,33 @@ contract PrescryptiveSmartContract is AccessControl {
     }
 
     /**
-     * @dev - Withdraws funds from Aave then to the msg.sender. Only works if the withdraw has been initated by the WITHDRAW_ROLE
+     * @dev - Withdraws funds from Aave then to the msg.sender. Only works if the withdraw has been initated by the WITHDRAW_ROLE. Double confirms the value and toPay to ensure that no malicious
+     * WITHDRAW_ROLE could change the address and value mid-execution
      */
-    function withdrawFunds(bool _withdraw) public onlyRole(CONFIRM_WITHDRAW_ROLE) {
+    function withdrawFunds(
+        bool _withdraw,
+        uint256 _value,
+        address _toPay
+    ) public onlyRole(CONFIRM_WITHDRAW_ROLE) {
         require(withdrawInitated); //requires that the withdraw has been initated
+        require(_value == withdrawValue && _toPay == withdrawAddress); //Ensures that the value and payment are the same, giving double-check on payment confirmation
 
-        //immediately set to false to prevent double-spend
+        //immediately set to defaults to follow checks-effects-interactions
         withdrawInitated = false;
-
-        if (_withdraw) {
-        //TODO - Add Aave integration (not possible until testnet release)
-        stablecoin.transfer(withdrawAddress, withdrawValue); //withdraws to the previously defined address and value
-        }
-
-        //defaults the withdraw values
         withdrawAddress = owner;
         withdrawValue = 0;
+
+        if (_withdraw) {
+            //TODO - Add Aave integration (not possible until testnet release)
+            stablecoin.transfer(_toPay, _value); //withdraws to the previously defined address and value
+        }
     }
 
     /**
      * @dev - The backdoor withdraw feature for the admin/owner address
      */
-    function ownerWithdraw(uint _value) public onlyRole(DEFAULT_ADMIN_ROLE) {
+    function ownerWithdraw(uint256 _value) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        //TODO - Add aave integration
         stablecoin.transfer(msg.sender, _value);
     }
-
-
-
-
 }
-
